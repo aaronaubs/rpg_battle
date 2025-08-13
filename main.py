35c0b5eba93bdd308c13640170feb7804f1a9612
aaronaubs <aaubs4@gmail.com>
2025-08-13 17:44:01 -0400

    Add function to introduce party, add map traversal loop for gameplay
    Introduces party after naming members. Map
    traversal logic fully operational.

--- main.py
+++ main.py
@@ -1,65 +1,16 @@
-from characters import Party, Warrior, Thief, White_Mage, Black_Mage, Forest_Wolf, Forest_Goblin
 import locations as loc
+from characters import hero_party
 from time import sleep
 
-
-hero_party = [
-    Warrior("Dante"),
-    Thief("Alita"),
-    White_Mage("Kara"),
-    Black_Mage("Gerald")
-]
-
-enemy_party = [
-    Forest_Wolf(),
-    Forest_Wolf(),
-    Forest_Goblin(),
-    Forest_Goblin()
-]
-
-def battle(party, enemies):
-    round_num = 1
-
-    while any(hero.is_alive() for hero in party) and any(enemy.is_alive() for enemy in enemies):
-        print(f"\n========== Round {round_num} ==========")
-        sleep(0.3)
-        print("\n----Your party's turn----")
+def show_hero_party():
+    print("\nYour party:")
+    print("-" * 40)
+    for hero in hero_party:
+        print(hero)
         sleep(0.5)
-        for hero in party:
-            if hero.is_alive():
-                if isinstance(hero, White_Mage):
-                    hero.choose_action(party, enemies)
-                else:
-                    hero.choose_action(enemies)
-
-        if any(enemy.is_alive() for enemy in enemies):
-            print("\n----Enemy party's turn----")
-            sleep(0.5)
-            for enemy in enemies:
-                if enemy.is_alive():
-                    enemy.attack_at_random(party)
-        # if no enemies are alive, break out of the loop
-        else:
-            break
-        
-        round_num += 1
-
-    if any(hero.is_alive() for hero in party):
-        print("*" * 30)
-        print("Victory!")
-        print("*" * 30)
-        sleep(0.5)
-        Party.gain_gold(enemies)
-        for hero in party:
-            if hero.is_alive():
-                hero.gain_exp(enemies)
-                hero.check_level_up()
-        print("*" * 30)        
-    else:
-        print("*" * 30)
-        print("Your party has been defeated...")
-        print("*" * 30)
+    print("-" * 40)    
 
 
 if __name__ == "__main__":
-    battle(hero_party, enemy_party)
+    show_hero_party()
+    loc.traverse_map()
